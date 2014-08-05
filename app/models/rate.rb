0c48946d886bdfe58e3e06759b5c079aa17cf55f
Mohamed H. Abdelalim <mohamedhussam29@gmail.com>
2014-08-05 14:23:17 +0200

    update

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,13 @@
 class Rate < ActiveRecord::Base
+   def create
+    Rate.create(rate_params)
+  end
+
+  private
+
+  def rate_params
+    params.require(:rate).permit(:quality, :cleanliness, :dealing, :price, :punctuality, :Review)
+  end
+  
+  belongs_to :worker
 end
