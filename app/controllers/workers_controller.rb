d56580951cbc6de7d683ec86874a8ca4ee49b286
Allessandra Ehab <allessandra.ehab@gmail.com>
2014-08-06 15:08:28 +0200

    mmmm

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -32,7 +32,7 @@ end
 
     respond_to do |format|
       if @worker.save
-        format.html { redirect_to @worker, notice: 'Worker was successfully created.' }
+        format.html { redirect_to @worker, notice: 'تم اضافة الفني بنجاح' }
         format.json { render :show, status: :created, location: @worker }
       else
         format.html { render :new }
@@ -46,7 +46,7 @@ end
   def update
     respond_to do |format|
       if @worker.update(worker_params)
-        format.html { redirect_to @worker, notice: 'Worker was successfully updated.' }
+        format.html { redirect_to @worker, notice: 'تم اضافة الفني بنجاح' }
         format.json { render :show, status: :ok, location: @worker }
       else
         format.html { render :edit }
@@ -60,7 +60,7 @@ end
   def destroy
     @worker.destroy
     respond_to do |format|
-      format.html { redirect_to workers_url, notice: 'Worker was successfully destroyed.' }
+      format.html { redirect_to workers_url, notice: 'تم ازالة الفني ' }
       format.json { head :no_content }
     end
   end
