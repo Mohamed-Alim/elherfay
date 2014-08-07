cdb369691a089e56db8717d6995599d2621de1a7
Mohamed H. Abdelalim <mohamedhussam29@gmail.com>
2014-08-07 12:32:13 +0200

    update

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -9,12 +9,29 @@ validates_associated :rates
 
 
 def total_average
-    x = Rate.where(:worker_id => self.worker.id).average('price')
-    y = Rate.where(:worker_id => self.worker.id).average('quality')
-    z = Rate.where(:worker_id => self.worker.id).average('punctuality')
-    m = Rate.where(:worker_id => self.worker.id).average('dealing')
-    n = Rate.where(:worker_id => self.worker.id).average('cleanliness')
+    x = Rate.where(:worker_id => self.id).average('price')
+    y = Rate.where(:worker_id => self.id).average('quality')
+    z = Rate.where(:worker_id => self.id).average('punctuality')
+    m = Rate.where(:worker_id => self.id).average('dealing')
+    n = Rate.where(:worker_id => self.id).average('cleanliness')
     average = (x+y+z+m+n)/5
   end
+  
+def average_price
+  x = Rate.where(:worker_id => self.id).average('price')
+end
+def average_quality
+  y = Rate.where(:worker_id => self.id).average('quality')
+end
+def average_punctuality
+  z = Rate.where(:worker_id => self.id).average('punctuality')
+end
+def average_dealing
+  m = Rate.where(:worker_id => self.id).average('dealing')
+end
+def average_cleanliness
+  n = Rate.where(:worker_id => self.id).average('cleanliness')
+end
+  
 end
 
